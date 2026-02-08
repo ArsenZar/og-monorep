28289dc67300839644b6813ef62de507d39574a1
ArsenZar <arsenzar@gmail.com>
2026-02-08 18:48:52 +0200

    test in Postman first endpoint

--- apps/api/src/modules/invites/invites.service.ts
+++ apps/api/src/modules/invites/invites.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
-import * as crypto from 'crypto';
+import { randomBytes } from 'crypto';
 
 @Injectable()
 export class InvitesService {
   constructor(private prisma: PrismaService) {}
 
+  private generateToken() {
+    return randomBytes(32).toString('hex');
+  }
+
   async createInvite(email: string, roleId: string, organizationId: string) {
-    const token = crypto.randomBytes(32).toString('hex');
+    const token = this.generateToken();
 
-    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
-
-    const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24);
+    const expiresAt = new Date();
+    expiresAt.setHours(expiresAt.getHours() + 48);
 
     await this.prisma.invite.create({
       data: {
         email,
-        token: hashedToken,
+        token,
         roleId,
         organizationId,
         expiresAt,
